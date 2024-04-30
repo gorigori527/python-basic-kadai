d1f51d297c04417505752e4ef5b0a7300d8c9b9c
gorigori527 <99218452+gorigori527@users.noreply.github.com>
2024-04-30 23:54:41 +0300

    Add files via upload

--- kadai_011/kadai_011.py
+++ kadai_011/kadai_011.py
@@ -1,6 +1,9 @@
 array = ["水","金","地","火","木","土","天","海","冥"]
-count = 1
-while count<=2:
-  for i in array:
-    print(i)
-  count = count + 1
+
+for i in array:
+  print(i)
+
+count = 0
+while count < 9:
+  print(array[count])
+  count = count+1
